024ae605c3b9ec30621029766c93fbc3b7c197fd
intolo <ssdoffice44@gmail.com>
2022-11-14 12:58:00 +0530

    storage link

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\DashboardController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/login');
 });
 Route::get('/category', function () {
     return view('category');
