85f271682a71443b3c63a748347a7b8e9eb01e0f
intolo <ssdoffice44@gmail.com>
2022-11-15 12:08:57 +0530

    login set

---
+++ app/MyApp.php
@@ -0,0 +1,28 @@
+<?php
+namespace App;
+
+class MyApp {
+    const SITE_NAME = 'E-Management';
+
+    const ADMINISTRATOR = 1;
+    const BILLING = 2;
+
+    const ACTIVE = 1;
+    const INACTIVE = 0;
+
+    const PENDING = 0;
+    const PICK_UP = 1;
+    const READY = 2;
+    const SERVED = 3;
+
+    const ONLINE = 1;
+    const CASH = 2;
+
+    const KG = 1;
+    const LITER = 2;
+    const PCS =3;
+    
+    const READY_MADE = 6;
+
+
+}
