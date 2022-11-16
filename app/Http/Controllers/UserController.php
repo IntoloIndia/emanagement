ec83d3feb92e37434061b06aef66e9ddeed607c6
rahul nath <rahul.nath@intoloindia.com>
2022-11-16 11:14:08 +0530

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        $getAllRole = Role::all();
+        // print_r($getAllRole);
+        return view('role',[
+            'getAllRole'=>$getAllRole
+        
+        ]);
+    }
+    
 }
